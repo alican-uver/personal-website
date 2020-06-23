4bfc04a51e42ecffd1e62b53d4e733788d33fef1
Alican Uver <can.uverr@gmail.com>
2020-06-23 18:34:41 +0300

    footer component written

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
 
 const Footer = () => {
-    return (
-        <>
-            hello from footer
-        </>
-    )
-}
+  return (
+    <Row className="bg-light px-3 text-center align-items-center" >
+      <Col>&copy; {new Date().getFullYear()} Tüm hakları saklıdır</Col>
+      <Col className="text-center">
+        <h4>E-Mail</h4>
+        <p className="">can.uverr@gmail.com</p>
+      </Col>
+    </Row>
+  );
+};
 
 export default Footer;
