0e2c49ad856abf44cc54096c3b1882f305bdaa4c
Alican Uver <can.uverr@gmail.com>
2020-06-23 18:35:18 +0300

    little updates

--- src/components/Home/ProfileCard.js
+++ src/components/Home/ProfileCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import "./profileCard.scss";
 import profileImg from "../../images/profile_images/profile_photo.jpeg";
+import Button from "../Button";
 
 const ProfileCard = () => {
   return (
@@ -17,22 +18,38 @@ const ProfileCard = () => {
           junior front end developer
         </p>
         <div className="social-media-container">
-          <a className ="pr-4" href="https://www.instagram.com/alicanuverrr/?hl=tr">
-            <i class="fab fa-instagram"></i>
+          <a
+            className="pr-4"
+            href="https://www.instagram.com/alicanuverrr/?hl=tr"
+          >
+            <i className="fab fa-instagram"></i>
           </a>
-          <a className ="pr-4" href="https://www.linkedin.com/in/alicanuver/">
-            <i class="fab fa-linkedin-in"></i>
+          <a className="pr-4" href="https://www.linkedin.com/in/alicanuver/">
+            <i className="fab fa-linkedin-in"></i>
           </a>
-          <a className ="pr-4" href="https://stackoverflow.com/users/12348877/canuver">
-            <i class="fab fa-stack-overflow"></i>
+          <a
+            className="pr-4"
+            href="https://stackoverflow.com/users/12348877/canuver"
+          >
+            <i className="fab fa-stack-overflow"></i>
           </a>
           <a href="https://github.com/alican-uver">
-            <i class="fab fa-github"></i>
+            <i className="fab fa-github"></i>
           </a>
         </div>
       </Col>
       <Col md={3} className="card-container-right">
         <p className="card-container-right-title text-uppercase">merhaba</p>
+        <p className="text-capitalize">ben kimim & neler yapıyorum</p>
+        <Button name="Özgeçmiş" />
+        <Button name="Projelerim" />
+        <p className="card-container-right-info pt-4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          quibusdam voluptatem neque dolore commodi voluptate facilis nam
+          similique iure aperiam numquam dolor asperiores atque ipsa omnis,
+          sapiente tenetur optio earum! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. 
+        </p>
       </Col>
     </Row>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,9 +3,13 @@ import { Container } from "react-bootstrap";
 import "./home.scss";
 import ProfileCard from "../../components/Home/ProfileCard";
 
+const homeContainer = {
+  background: 'linear-gradient(to right, #e6dace 40%, #fff 40%, #fff 100%)',
+}
+
 const Home = () => {
   return (
-    <Container fluid className="home-container">
+    <Container fluid className="home-container" style ={homeContainer}>
         <ProfileCard />
     </Container>
   );
